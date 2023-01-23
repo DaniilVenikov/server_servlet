057415abef64ed78fecb36d026b1aebae0358d14
DaniilVenikov <daniilvenikoff@gmail.com>
2023-01-24 00:55:46 +0300

    Fix repository

--- src/main/java/repository/PostRepository.java
+++ src/main/java/repository/PostRepository.java
@@ -6,24 +6,44 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 // Stub
 public class PostRepository {
 
     private static final List<Post> repository = Collections.synchronizedList(new ArrayList<>());
+
     public List<Post> all() {
         return repository;
     }
 
-    public Optional<Post> getById(long id)
-    {
-        return Optional.empty();
+    public Optional<Post> getById(long id) {
+        return repository.stream()
+                .filter((post) -> post.getId() == id)
+                .findFirst();
     }
 
     public Post save(Post post) {
+        boolean flag = false;
+        for (Post p : repository) {
+            if (p.getId() == post.getId()) {
+                p.setContent(post.getContent());
+                flag = true;
+                break;
+            }
+        }
+        if(!flag){
+            repository.add(post);
+        }
         return post;
     }
 
     public void removeById(long id) {
+        for (int i = 0; i < repository.size(); i++) {
+            if(repository.get(i).getId() == id){
+                repository.remove(i);
+                break;
+            }
+        }
     }
 }
